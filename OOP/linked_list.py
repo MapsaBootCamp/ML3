c0f32acc96339ad5b24e634ff69f71d21bec93ea
teghfo <teghfo@gmail.com>
2022-11-04 14:17:02 +0330

    linked list

---
+++ OOP/linked_list.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.next_node = None
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.root = None
+
+    def add_begin(self, node):
+        pass
+
+    def append(self, node):
+        pass
+
+    def pop(self):
+        pass
+
+    def remove(self, index):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
